b1b0bb7964e0292b926c4fe760c40e18c8ad2cd9
melekdogan <48091092+melekdogan@users.noreply.github.com>
2021-10-10 22:25:31 +0300

    Method Defining

---
+++ MethodDefining/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDefining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2, b = 3;
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a, b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttırVeTopla(a, b);
+            ornek.EkranaYazdir(sonuc.ToString());
+            ornek.EkranaYazdir((a + b).ToString());
+            Console.ReadKey();
+        }
+
+
+        static int Topla(int deger1, int deger2)
+        {
+            return deger1 + deger2;
+        }
+    }
+
+    class Metotlar
+    {
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttırVeTopla(int deger1, int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+        }
+    }
+}
