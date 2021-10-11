a3338f4d65be4e8701a38b1accef680c21586aee
melekdogan <48091092+melekdogan@users.noreply.github.com>
2021-10-11 17:06:03 +0300

    Enums

---
+++ Enumeration/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Enumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int) Gunler.Cumartesi);
+
+            int sicaklik = 32;
+            if (sicaklik <= (int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Sicak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");
+            }
+            else
+            {
+                Console.WriteLine("Hadi dışarıya çıkalım!");
+            }
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi=1,
+        Sali,
+        Carsamba, 
+        Persembe, 
+        Cuma=23,
+        Cumartesi, 
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk=5,
+        Normal=20,
+        Sicak=25,
+        CokSicak=30
+    }
+}
