bb0505e0b50588911ec4e514718af32191d9e2ff
melekdogan <48091092+melekdogan@users.noreply.github.com>
2021-10-12 13:59:31 +0300

    Koleksiyonlarla ilgili algoritma soruları

---
+++ Koleksiyonlar-Soru3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar_Soru3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+            char[] vowels = { 'a','e','ı','i','u','ü','o','ö'}; //sesli harfler sabit olduğu için, bu harflerin içinde olduğu bir dizi tanımladım. 
+            int count = 0;
+            string vowelsInInput = "";
+            List<char> vowelList = new List<char>();
+            Console.WriteLine("bir cümle giriniz:");
+            String input = Console.ReadLine().ToLower(); 
+           
+            for (int i = 0; i < input.Length; i++) 
+            {
+                for (int k = 0; k < vowels.Length; k++) 
+                {
+                    if (input[i] == vowels[k]) 
+                    {
+                        vowelsInInput+=input[i];
+                        count++; 
+                    }
+                }
+            }
+
+            for(int i = 0; i < vowelsInInput.Length; i++)
+            {
+                vowelList.Add(vowelsInInput[i]);
+            }
+
+            vowelList.Sort();
+
+            foreach (char item in vowelList)
+            {
+                Console.Write(item+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"sesli harf sayısı: {count}"); 
+            Console.ReadKey();
+        }
+
+    }
+    }
+
