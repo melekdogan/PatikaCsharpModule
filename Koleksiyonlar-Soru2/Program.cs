c251190266f20db615f7ace32c9a9d6f7434a6fa
melekdogan <48091092+melekdogan@users.noreply.github.com>
2021-10-12 12:25:01 +0300

    Koleksiyonlarla ilgili algoritma soruları

---
+++ Koleksiyonlar-Soru2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Koleksiyonlar_Soru2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Soru - 2: Klavyeden girilen 20 adet sayının en büyük 3 tanesi ve en küçük 3 tanesi bulan, her iki grubun kendi içerisinde ortalamalarını alan ve bu ortalamaları ve ortalama toplamlarını console'a yazdıran programı yazınız.
+            //(Array sınıfını kullanarak yazınız.)
+            int[] numbers = new int[20];
+            int[] smallest = new int[3];
+            int[] largest = new int[3];
+            decimal averageOfLargestNumbers ;
+            decimal averageOfSmallestNumbers;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{i+1}. Sayıyı Girin: ");
+                numbers[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(numbers);//Küçükten büyüğe sıralandı
+            
+            for(int i = 0; i < smallest.Length; i++)
+            {
+                smallest[i] = numbers[i];
+            }
+            Array.Reverse(numbers); // Ters çevrilerek büyükten küçüğe sıralanmış oldu. Çünkü önceki satırlarda zaten küçükten büyüğe sıralanmıştı.
+            for (int i = 0; i < largest.Length; i++)
+            {
+                largest[i] = numbers[i];
+            }
+            averageOfLargestNumbers = Average(largest);
+            averageOfSmallestNumbers = Average(smallest);
+
+            Console.WriteLine($"En büyük 3 sayının ortalaması: {averageOfLargestNumbers}");
+            Console.WriteLine($"En Küçük 3 sayının ortalaması: {averageOfSmallestNumbers}");
+            Console.WriteLine($"Bu Ortalamaların toplamı {averageOfSmallestNumbers+averageOfLargestNumbers}");
+        }
+        static decimal Average(int[] arr)
+        {
+            int sum = 0;
+            foreach (int item in arr)
+            {
+                sum += item;
+            
+            }
+            return (sum / arr.Length);
+        }
+    }
+}
